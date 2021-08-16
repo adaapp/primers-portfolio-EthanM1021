0b725c1d2bd1c1aa80a7e34c820d057c9adeb5e3
EthanM1021 <79159281+EthanM1021@users.noreply.github.com>
2021-08-16 15:53:39 +0000

    Completing temperature converter

--- include/monday.h
+++ include/monday.h
@@ -1,5 +1,27 @@
+#include <iostream>
+using namespace std;
+
 void fahrenheitCentigradeConversion(void) {
-	std::cout << "Complete this first!\n\n";
+  float temperature;
+  char choice;
+
+	std::cout << "Please enter the starting temperature: ";
+  
+  std::cin >> temperature;
+
+  std::cout << "\nPress 'C' to convert from Fahrenheit to Centigrade...";
+  std::cout << "\nPress 'F' to convert from Centigrade to Fahrenheit...\n";
+
+  std::cin >> choice;
+  std::cout << "You picked: " << choice << endl;
+
+  if (choice == 'F' || choice == 'f') {
+    std::cout << temperature << " in Centigrade is " << (temperature * 9 / 5) + 32 << endl;
+  } else if (choice == 'C' || choice == 'c') {
+    std::cout << temperature << " in Fahrenheit is " << (temperature - 32) * 5 / 9 << endl;
+  } else {
+    std::cout << "You need to enter either 'C' or 'F'"; 
+  }
 }
 
 
