0f3c53b442fbf0c93f90ad12459854878da71edc
EthanM1021 <79159281+EthanM1021@users.noreply.github.com>
2021-08-17 12:37:00 +0000

    Turned conversions into pure functions

--- include/monday.h
+++ include/monday.h
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 
+float FtoC(float temp) {
+  return ((temp - 32) * 5 / 9);
+}
+
+float CtoF(float temp) {
+  return ((temp * 9 / 5) + 32);
+}
+
 void fahrenheitCentigradeConversion(void) {
   float temperature;
   char choice;
@@ -16,13 +24,15 @@ void fahrenheitCentigradeConversion(void) {
   std::cout << "You picked: " << choice << endl;
 
   if (choice == 'F' || choice == 'f') {
-    std::cout << temperature << " in Centigrade is " << (temperature * 9 / 5) + 32 << endl;
+    std::cout << temperature << " in Centigrade is " << CtoF(temperature) << endl;
   } else if (choice == 'C' || choice == 'c') {
-    std::cout << temperature << " in Fahrenheit is " << (temperature - 32) * 5 / 9 << endl;
+    std::cout << temperature << " in Fahrenheit is " << FtoC(temperature) << endl;
   } else {
     std::cout << "You need to enter either 'C' or 'F'"; 
   }
 }
+
+
 
 
 void selfServiceCheckout(void) {
