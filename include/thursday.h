04a126fcc59342f8e2c557f18e5dc9434592dd84
EthanM1021 <79159281+EthanM1021@users.noreply.github.com>
2021-08-19 18:11:19 +0000

    Numero 8

--- include/thursday.h
+++ include/thursday.h
@@ -1,6 +1,9 @@
 #include <iostream>
 #include<thread>
 
+const int t1Duration = 5;
+const int t2Duration = 10;
+
 void sleep(int seconds = 10) {
   int milliseconds = seconds * 1000; 
   
@@ -12,7 +15,48 @@ int sleepTimer(int seconds = 10) {
   return 0;
 }
 
+void timer1() {
+  std::cout << "Thread 1 - " << std::this_thread::get_id() << " started\n";
+  sleep(t1Duration);
+  std::cout << "Thread 1 - " << std::this_thread::get_id() << " ended\n";
+}
+
+void timer2() {
+  std::cout << "Thread 2 - " << std::this_thread::get_id() << " started\n";
+  sleep(t2Duration);
+  std::cout << "Thread 2 - " << std::this_thread::get_id() << " ended\n";
+}
+
+int joinThreads(void) {
+  std::cout << "\nJoin Threads\n\n";
+  std::cout << "Main thread: " << std::this_thread::get_id() << " started\n";
+
+  std::thread thread1(timer1); 
+  std::thread thread2(timer2); 
+
+  thread1.join(); 
+  thread2.join(); 
+
+  std::cout << "Main thread: " << std::this_thread::get_id() << " ended\n";
+
+  return 0;
+}
+
+
+int detachThreads(void) {
+  std::cout << "Main thread: " << std::this_thread::get_id() << " started\n";
+
+  std::thread thread1(timer1);
+  thread1.detach();
+
+  std::thread thread2(timer2); 
+  thread2.detach();
+
+  std::cout << "Main thread: " << std::this_thread::get_id() << " ended\n";
+
+  return 0;
+}
 
 void joinDetachThreads(void) {
-	std::cout << " - joinDetachThreads: not yet implemented\n\n";
+	detachThreads();
 }
